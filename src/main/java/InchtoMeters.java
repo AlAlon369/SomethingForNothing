b930a6524cb0a3127230dcc38ca217aa22e565ec
AlAlon <alalon211090@gmail.com>
2022-05-18 18:14:47 +0300

    Самая хуевая задача для новичков.

---
+++ src/main/java/InchtoMeters.java
@@ -0,0 +1,17 @@
+public class InchtoMeters {
+    public static void main(String[] args) {
+        double meter, inch;
+        int counter;
+
+        counter = 0;
+        for (inch = 1; inch <= 144; inch++) {
+            meter = inch * 39.37; // преборазование в метры
+            System.out.println(inch + " дюймы соответствует " + meter + " метра ");
+            counter++;
+            if (counter == 12) {
+                System.out.println();
+                counter = 0; // сбросить счетчик строк
+            }
+        }
+    }
+}
