2a67df899e138d42ff5f54a9d13302252d05c146
AlAlon <alalon211090@gmail.com>
2022-05-20 14:08:03 +0300

    Знакомства с типами данных и операторами. С символьными переменными можно обращаться как с целочисленными.

---
+++ src/main/java/CharArithDemo.java
@@ -0,0 +1,18 @@
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+        ch = 'X';
+
+
+        System.out.println("ch содержит " + ch);
+
+        ch++; // инкриментировать переменную ch
+        System.out.println("теперь ch содержит " + ch);
+
+        ch = 90; // присвоить переменной ch значение 'Z'
+        System.out.println("Теперь ch содержит " + ch);
+
+        // 'Z' - циклическое значение.
+
+    }
+}
