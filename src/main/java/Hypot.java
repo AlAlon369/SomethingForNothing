56a5345ebee051dc31a4df972bbb03cfa6c7f8e2
AlAlon <alalon211090@gmail.com>
2022-05-20 13:50:11 +0300

    Типы данных с плавающей точкой. Определение длинны гипотенузы исходя из длинны катетов, по теореме Пифагора.

---
+++ src/main/java/Hypot.java
@@ -0,0 +1,12 @@
+public class Hypot {
+    public static void main(String[] args) {
+        double x, y, z;
+        x = 3;
+        y = 4;
+                                        // Саначало указываем имена классов, а потом метод.
+        z = Math.sqrt (x*x + y*y);      // метод sqrt определен в стандартном классе Math.
+
+        System.out.println("Длинна гипотенузы: " +z);      // System.out. - метод класса. println - метод
+
+    }
+}
