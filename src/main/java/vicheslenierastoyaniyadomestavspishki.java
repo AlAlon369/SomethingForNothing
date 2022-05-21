61bf69b81f0fefc0586c58bd63a0fbe3193b8884
AlAlon <alalon211090@gmail.com>
2022-05-21 17:09:20 +0300

    Упражнение 2.1 Вычесление расстояния до места вспышки молнии

---
+++ src/main/java/vicheslenierastoyaniyadomestavspishki.java
@@ -0,0 +1,11 @@
+public class vicheslenierastoyaniyadomestavspishki {
+    public static void main(String[] args) {
+        double dist;
+        dist = 1100 * 7.2;
+
+        System.out.println("Растояние до места вспышки молнии " + "состоявляет " + dist + " футов");
+
+        // Вычесление расстояния до места вспышки молнии, звук от которого доходит до наблюдателя через 7,2 сек
+    }
+
+}
