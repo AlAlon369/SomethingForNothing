793a8828a3927c9be70c6ac97befedfd6a8fde91
AlAlon <alalon211090@gmail.com>
2022-05-30 22:30:03 +0300

    Какая ошибка допущена в данном фрагменте кода?

---
+++ src/main/java/nvm2.java
@@ -0,0 +1,12 @@
+public class nvm2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            int sum;
+
+            sum = sum + i;
+
+            System.out.println("Сумма: " + sum);
+        }
+    }
+}
