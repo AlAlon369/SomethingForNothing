c269831631f0bc7be70f7ccc90ca4283725c4534
AlAlon <alalon211090@gmail.com>
2022-06-04 22:48:58 +0300

    makeNegative reshenie return -number; ggs.

---
+++ src/main/java/Kata2.java
@@ -0,0 +1,21 @@
+public class Kata2 {
+    public static void main(String[] args) {
+        System.out.println(makeNegative(1));
+        System.out.println(makeNegative(14));
+        System.out.println(makeNegative(-34));
+    }
+
+    public static int makeNegative(final int x) {
+        if (x <= 1) {
+            return -x;
+        }
+        if (x <= 14) {
+            return -x;
+        }
+        if (x >= -34) ;
+        {
+            return +x;        // РЕШЕНИЕ: просто пиши return -number;
+        }
+    }
+}
+
