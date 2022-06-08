6402257b295319b103933b629b44cc4269037453
AlAlon <alalon211090@gmail.com>
2022-06-08 17:57:08 +0300

    Тело цикла for может быть пустым

---
+++ src/main/java/Empty3.java
@@ -0,0 +1,11 @@
+public class Empty3 {
+    public static void main(String[] args) {
+        int i;
+        int sum = 0;
+
+        // суммируются числа от 1 до 5
+        for (i = 1; i <= 5; sum += i++) ;  // В цикле отсутсвутет тело!
+
+        System.out.println("Сумма: " + sum);
+    }
+}
