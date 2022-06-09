9197ad67e3728bf83205652c3131bc73d1e8a465
AlAlon <alalon211090@gmail.com>
2022-06-09 19:02:45 +0300

    Вычисление целых степеней числа 2. Цикл for, цикл while

---
+++ src/main/java/power.java
@@ -0,0 +1,17 @@
+public class power {
+        public static void main(String[] args) {
+            int e;
+            int result;
+
+            for (int i = 0; i < 10; i++) {
+                result = 1;
+                e = i;
+                while (e > 0) {
+                    result *= 2;
+                    e--;
+                }
+                System.out.println("2 в степени " + i + " равно " + result);
+            }
+        }
+    }
+}
