71fa6a83f76748957fabd05d487c33cd7a7df66a
AlAlon <alalon211090@gmail.com>
2022-06-12 17:49:47 +0300

    Чтение вводимых данных до тех пор, пока не будет получена буква q

---
+++ src/main/java/Others/Break2.java
@@ -0,0 +1,16 @@
+package Others;
+
+public class Break2 {
+    public static void main(String[] args)
+        throws java.io.IOException {
+
+        char ch;
+
+        for ( ; ;) {       // < --- бесконечный цикл, щавершаемый инструкцией break
+            ch = (char) System.in.read();  // получение символа с клавиатуры
+
+            if (ch == 'q') break;   // // < --- бесконечный цикл, щавершаемый инструкцией break
+        }
+        System.out.println("Вы нажали клавишу q!");
+    }
+}
