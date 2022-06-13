f04d17270eae6fcc24770ce2f2f2ebab7f714945
AlAlon <alalon211090@gmail.com>
2022-06-13 20:37:42 +0300

    Zaur's Lesson 6, MethodOverloading: Перезагруженные методы имеют одинаковые имена и разный список параметров.

---
+++ src/main/java/Zaur/lesson6/MethodOverloading.java
@@ -0,0 +1,33 @@
+package Zaur.lesson6;
+
+public class MethodOverloading {
+    
+    void show(int i1) {
+        System.out.println(i1);
+    }
+
+    void show(boolean b1) {
+        System.out.println(b1);
+    }
+
+    void show(String s1) {
+        System.out.println(s1);
+    }
+}
+
+
+
+
+class MethodOverloadingtest{
+    public static void main(String[] args) {
+        MethodOverloading mO = new MethodOverloading();
+        int a = 500;
+        mO.show(a);
+        boolean b = true;
+        mO.show(b);
+        String s = "Privet!!!";
+        mO.show(s);
+    }
+}
+
+
