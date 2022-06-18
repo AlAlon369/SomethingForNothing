ece8ab20b1a05debc002c8ccbe9a0fb9cd3f0039
AlAlon <alalon211090@gmail.com>
2022-06-18 19:23:32 +0300

    Программа использующая цикл for для вывода чисел в геометрической прогрессии: 1, 2, 4, 8, 16, 32, 64

---
+++ src/main/java/Others/exam3.java
@@ -0,0 +1,11 @@
+package Others;
+
+public class exam3 {
+    public static void main(String[] args) {
+
+            for (int i = 1; i < 100; i += i)
+            System.out.print(i + " ");
+        }
+    }
+
+
