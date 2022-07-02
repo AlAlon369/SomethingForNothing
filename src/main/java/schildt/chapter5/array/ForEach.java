13d1962b259821207e6e3ac8059179bc80e4ed23
AlAlon <alalon211090@gmail.com>
2022-07-02 15:51:31 +0300

    Использование цикла типа for-each

---
+++ src/main/java/schildt/chapter5/array/ForEach.java
@@ -0,0 +1,18 @@
+package schildt.chapter5.array;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+
+        // Использование цикла типа for-each для суммирования и отображения значений.
+        for (int x : nums)   {         // Цикл типа for-each
+            System.out.println("Значение: " + x);
+            sum += x;
+        }
+        System.out.println("Сумма: " + sum);
+
+
+
+    }
+}
