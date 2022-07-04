d4e116c766867b2272fa3df9abf02e07213a5ad0
AlAlon <alalon211090@gmail.com>
2022-07-04 17:10:36 +0300

    Различные способы объявления строк

---
+++ src/main/java/schildt/chapter5/array/StringDemo.java
@@ -0,0 +1,16 @@
+package schildt.chapter5.array;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        // различные способы объявления строк
+        String str1 = new String ("В Java строки - это объекты.");
+        String str2 = "Их можно создавать разными способами.";
+        String str3 = new String (str2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
