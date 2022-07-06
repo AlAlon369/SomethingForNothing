d674848484281a96426bcf3870909fd6f2e4b912
AlAlon <alalon211090@gmail.com>
2022-07-06 15:27:19 +0300

    WON!

---
+++ src/main/java/Codewars/kata25.java
@@ -0,0 +1,27 @@
+package Codewars;
+
+import java.util.Objects;
+
+public class kata25 {
+    public static void main(String[] args) {
+        System.out.println(greet("Daniel", "Daniel"));
+        System.out.println(greet("Greg", "Daniel"));
+    }
+
+    // СОздать функцию которая будет вылавать персонализированное приветстввие, принимающая в себя два параметра
+    // name и hello.
+    // Имя равно владельцу "Hello boss"
+    // В другом случае "Hello guest"
+
+    static String greet(String name, String owner) {
+        if (name.equals(owner)) {
+            return "Hello boss";
+        } else {
+            return "Hello guest";
+   //      if (Objects.equals(name, owner)) {
+   //         return "Hello boss";
+   //     } else {
+   //         return "Hello quest";
+        }
+    }
+}
