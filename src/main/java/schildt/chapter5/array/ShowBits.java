7949c48d9c16c72edfda7ff59f33e4c1d45983cf
AlAlon <alalon211090@gmail.com>
2022-07-19 13:51:35 +0300

    Создание класса для отображения значений в двоичном виде

--- src/main/java/schildt/chapter5/array/ShowBits.java
+++ src/main/java/schildt/chapter5/array/ShowBits.java
@@ -1,14 +1,51 @@
 package schildt.chapter5.array;
 
 public class ShowBits {
-    public static void main(String[] args) {
-        int t;
-        byte val;
+    int numbits;
 
-        val = 123;
-        for (t = 128; t > 0; t = t/2) {
-            if ((val & t) != 0) System.out.print("1 ");
-            else System.out.print("0 ");
+    ShowBits(int n) {
+        numbits = n;
+    }
+
+    void show(long val) {
+        long mask = 1;
+
+        // Сдвиг значения 1 влево на нужную позицию
+        mask <<= numbits - 1;
+
+        int spacer = 0;
+        for (; mask != 0; mask >>>= 1) {
+            if ((val & mask) != 0) System.out.print("1");
+            else System.out.print("0");
+            spacer++;
+            if ((spacer & 8) == 0) {
+                System.out.print(" ");
+                spacer = 0;
+            }
         }
+        System.out.println();
+
+    }
+}
+
+// Демонстрация использования класса ShowBits
+class ShowBitsDemo {
+    public static void main(String[] args) {
+        ShowBits b = new ShowBits(8);
+        ShowBits i = new ShowBits(32);
+        ShowBits li = new ShowBits(64);
+
+        System.out.println("123 в двоичном представлении: ");
+        b.show(123);
+
+        System.out.println("\n87987 в двоичном представлении: ");
+        i.show(87987);
+
+        System.out.println("\n237658768 в двоичном представлении: ");
+        li.show(237658768);
+
+        // Можно также отобразить младшие разряды любого целого числа
+        System.out.println("\nМладшие 8 битов числа 87987 в двоичном представлении: ");
+        b.show(87987);
     }
 }
