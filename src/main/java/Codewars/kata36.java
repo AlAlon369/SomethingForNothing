5d3c086226840dd2f317ec94f018b9e2717aabc8
AlAlon <alalon211090@gmail.com>
2022-07-25 13:55:44 +0300

    highAndLow kata solved

---
+++ src/main/java/Codewars/kata36.java
@@ -0,0 +1,41 @@
+package Codewars;
+
+public class kata36 {
+    public static void main(String[] args) {
+        System.out.println(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        System.out.println(highAndLow("1 2 3"));
+    }
+    // Нам дан массив разделенных пробелом числе.
+    // Нужно вернуть наименьшее и наибольшое число.
+
+    // План:
+    // Обратиться к массиву чисел.
+    // Пробежаться по массиву, возвратить наименьшее и наибольшее число.
+
+    // 1. Сплитнуть стрингу (получу массив строк)
+    // 2. Создать переменные int min, int max, и присваиваю им первый элемент массива. (Интеджерпарсинт)
+    // 3. Создаю цикл, прохожусь по массиву стрингов.
+    // 4. Если элемент меньше min - присваиваем в min этот элемент.
+    // 5. Если эжлемент больше max - присваиваем в max этот элемент.
+    // 6. Вернуть стрингу min пробел max + " " +
+
+
+    public static String highAndLow(String numbers) {
+        String strArray[] = numbers.split(" ");
+        final String firstElement = strArray[0];
+        int maximum = Integer.parseInt(firstElement);
+        int minimum = Integer.parseInt(firstElement);
+
+        for (String element : strArray) {
+            final int elementInteger = Integer.parseInt(element);
+            if (elementInteger > maximum) {
+                maximum = elementInteger;
+            }
+            if (elementInteger < minimum) {
+                minimum = elementInteger;
+            }
+        }
+        return Integer.parseInt(String.valueOf(maximum)) + " " + Integer.parseInt(String.valueOf(minimum));
+    }
+}
+
