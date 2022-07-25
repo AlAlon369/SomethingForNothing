6b4e7033da2424c96c5575f49dd180015a9010be
AlAlon <alalon211090@gmail.com>
2022-07-25 13:59:57 +0300

    noBoringZeros solved.

--- src/main/java/Codewars/kata31.java
+++ src/main/java/Codewars/kata31.java
@@ -9,26 +9,10 @@ public class kata31 {
     }
 
 
-    // избавиться от нулей.
-    // от конечных нулей.
-
     public static int noBoringZeros(int n) {
-     //   return n / 10;
+        String number = String.valueOf(n);
+        String numberAfterReplace = number.replaceAll("[0]+$", "");
 
-         String number = String.valueOf(n);
-         String numberAfterReplace = number.replaceAll("[0]+$", "");
-
-         return Integer.parseInt(numberAfterReplace);
-
-
-
-        // if (n == 0) return n;
-        //
-        // while                   // посмотреть цикл вайл в интернете
+        return Integer.parseInt(numberAfterReplace);
     }
-
-
-
-
-
 }
