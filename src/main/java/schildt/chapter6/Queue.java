fbb9ca22da8a353280fd87c6d378448628e48c9f
AlAlon <alalon211090@gmail.com>
2022-07-26 18:19:00 +0300

    Усовершенствованный класс очереди, предназначенной для хранения символьных значений

---
+++ src/main/java/schildt/chapter6/Queue.java
@@ -0,0 +1,34 @@
+package schildt.chapter6;
+
+
+// Усовершенствованный класс очереди, предназначенной для хранения символьных значений
+
+class Queue {
+    // Эти члены класса теперь являются закрытыми
+    private char q[];   // Массив для хранения элементов очереди
+    private int putloc, getloc;   // индексы для вставки и извлечения элементов очереди
+
+    Queue(int size) {
+        q = new char[size];   // выделения памяти для очереди
+        putloc = getloc = 0;
+    }
+
+    // Помещение символа в очередь
+    void put(char ch) {
+        if (putloc == q.length - 1) {
+            System.out.println(" - Очередь заполнена.");
+            return;
+        }
+
+        q[putloc++] = ch;
+    }
+
+    // Извлечение символа из очереди
+    char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Очередь пуста.");
+            return (char) 0;
+        }
+        return q[getloc++];
+    }
+}
