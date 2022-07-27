def1d5f9d67e6101dba9eb8f0e40eafea236ef2b
AlAlon <alalon211090@gmail.com>
2022-07-27 17:00:05 +0300

    Возврат объекта типа String

---
+++ src/main/java/schildt/chapter6/ErrorMsg.java
@@ -0,0 +1,30 @@
+package schildt.chapter6;
+
+  // Возврат объекта типа String
+
+ class ErrorMsg {
+     String msgs[] = {
+             "Ошибка вывода",
+             "Ошибка ввода",
+             "отсутствует место на диске",
+             "Выход индекса за границы диапазона"
+     };
+
+     // Возврат сообщения об ошибке
+     String getErrorMsg(int i) {          // Возврат объекта типа String
+         if (i >= 0 & i < msgs.length)
+         return msgs[i];
+         else
+             return "Несуществующий код ошибки";
+     }
+}
+
+class ErrMsg {
+    public static void main(String[] args) {
+        ErrorMsg err = new ErrorMsg();
+
+        System.out.println(err.getErrorMsg(2));
+        System.out.println(err.getErrorMsg(18));
+
+    }
+}
