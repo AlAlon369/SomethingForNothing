12b5120ab1b75171153ea178e7e7710d3fd7c96c
alalon369 <alalon211090@gmail.com>
2022-09-01 16:43:36 +0300

    TwoDShape modified

--- src/main/java/schildt/chapter7/TwoDShape.java
+++ src/main/java/schildt/chapter7/TwoDShape.java
@@ -1,10 +1,18 @@
 package schildt.chapter7;
 
 public class TwoDShape {
-    double width;
-    double height;
+   private double width; // теперь эти переменные объявлены как закрытые
+   private double height;       // теперь эти переменные объявлены как закрытые
+
+    // Методы доступа к переменным экземпляра width и height
+
+    double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth (double w) { width = w; }
+    void setHeight (double h) { height = h; }
 
     void showDim() {
+
         System.out.println("Ширина и высота - " + width + " и " + height);
     }
 }
@@ -12,29 +20,30 @@ public class TwoDShape {
 // Подкласс для представления треугольников.
 // Производный от класса TwodShape
 class Triangle extends TwoDShape {
-    String style;
+    private String style;
 
+    // Конструктор
+    Triangle (String s, double w, double h) {
+        setWidth(w);
+        setHeight(h);                             // Инициализация части объекта, соответствующей класса TwoDShape
+
+        style = s;
+    }
     double area() {
-        return width * height / 2;      // Из класса Triangle можно обращаться к членам класса TwoDShape так, как если бы это были его собственные члены.
+        return getWidth() * getHeight() / 2;      // Из класса Triangle можно обращаться к членам класса TwoDShape так, как если бы это были его собственные члены.
     }
 
     void showStyle() {
+
         System.out.println("Треугольник " + style);
     }
 }
 
-class Shapes {
+class Shapes3 {
     public static void main(String[] args) {
-        Triangle t1 = new Triangle();
-        Triangle t2 = new Triangle();
+        Triangle t1 = new Triangle("закрашенный", 4.0, 4.0);
+        Triangle t2 = new Triangle("Контурныйй", 8.0, 12.0);
 
-        t1.width = 4.0;
-        t1.height = 4.0;                     // Объектам типа Triangle доступны все члены класса Triangle, даже те, которые унаследованы от класса TwoDShape
-        t1.style = "закрашенный";
-
-        t2.width = 4.0;
-        t2.height = 12.0;
-        t2.style = "контурный";
 
         System.out.println("Информация о t1: ");
         t1.showStyle();
@@ -54,11 +63,12 @@ class Shapes {
 // Подкласс для представления прямоугольников, производных от класса TwoDShape
 class Rectangle extends TwoDShape {
     boolean isSquare() {
-        if (width == height) return true;
+        if (getWidth() == getHeight()) return true;
         return false;
     }
 
     double area() {
-        return width * height;
+
+        return getWidth() * getHeight();
     }
 }
