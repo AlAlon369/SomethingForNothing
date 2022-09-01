41b4aa54d6e6aa9f028461be5f0fb16688b8c190
alalon369 <alalon211090@gmail.com>
2022-09-01 17:05:10 +0300

    Добовление дополнительных конструкторов в класс TwoDShape, используя метод super()

--- src/main/java/schildt/chapter7/TwoDShape.java
+++ src/main/java/schildt/chapter7/TwoDShape.java
@@ -1,7 +1,7 @@
 package schildt.chapter7;
 
 public class TwoDShape {
-   private double width; // теперь эти переменные объявлены как закрытые
+   private double width;        // теперь эти переменные объявлены как закрытые
    private double height;       // теперь эти переменные объявлены как закрытые
 
     // Методы доступа к переменным экземпляра width и height
@@ -9,6 +9,14 @@ public class TwoDShape {
     TwoDShape (double w, double h) {       // <-- Конструктор класса TwoDShape
         width = w;
         height = h;
+    }
+    // Конструктор, заданный по умолчанию             // Использование метода super() для вызова разных форм конструктора TwoDShape()
+    TwoDShape() {
+        width = height = 0.0;
+    }
+    // Создание объекта с одинаковыми значениями переменных экземпляра width и height
+    TwoDShape (double x) {
+        width = height = x;
     }
 
     double getWidth() { return width; }
@@ -27,12 +35,23 @@ public class TwoDShape {
 class Triangle extends TwoDShape {
     private String style;
 
-    // Конструктор
+    // Конструктор по умолчанию
+    Triangle() {
+        super();     // Вызов конструктора суперкласса по умолчанию
+        style = "nonme";
+    }
+    // конструктор
     Triangle (String s, double w, double h) {
-        super(w, h);     // Вызов конструктора суперкласса
-                         // Super - Использование оператора super() для вызова конструктора класса TwoDShape
+        super(w, h);     // Вызов конструктора суперкласса с двумя аргументами.
 
         style = s;
+    }
+
+    // Конструктор с одним аргументом
+    Triangle (double x) {
+        super(x);  // Вызов конструктора суперкласса с одним аргументом.
+
+        style = "закрашенный";
     }
     double area() {
         return getWidth() * getHeight() / 2;      // Из класса Triangle можно обращаться к членам класса TwoDShape так, как если бы это были его собственные члены.
@@ -44,10 +63,13 @@ class Triangle extends TwoDShape {
     }
 }
 
-class Shapes4 {
+class Shapes5 {
     public static void main(String[] args) {
         Triangle t1 = new Triangle("закрашенный", 4.0, 4.0);
         Triangle t2 = new Triangle("Контурныйй", 8.0, 12.0);
+        Triangle t3 = new Triangle (4.0);
+
+        t1 = t2;
 
 
         System.out.println("Информация о t1: ");
@@ -62,6 +84,16 @@ class Shapes4 {
         t2.showDim();
 
         System.out.println("Площадь - " + t2.area());
+
+        System.out.println();
+
+        System.out.println("Инфолрмация о t3: ");
+        t3.showStyle();
+        t3.showDim();
+        System.out.println("Площадь - " + t3.area());
+
+        System.out.println();
     }
 }
+
 
