d2362f593af496b1fc749d9806f40534c1d0254d
alalon369 <alalon211090@gmail.com>
2022-09-02 20:20:59 +0300

    kata 2 completed

--- src/main/java/Codewars/Kata2.java
+++ src/main/java/Codewars/Kata2.java
@@ -8,16 +8,16 @@ public class Kata2 {
     }
 
     public static int makeNegative(final int x) {
-        if (x <= 1) {
+        if (x > 1) {
             return -x;
         }
-        if (x <= 14) {
+        if (x < 14) {
             return -x;
         }
-        if (x >= -34) ;
-        {
-            return +x;        // решение: просто пиши return -number;
+        if (x > 34) {
+            return +x;
         }
+        return x;
     }
 }
 
