fdc5e4f243bb92d41bbf05d52fd7fecb9f3b0083
alalon369 <alalon211090@gmail.com>
2022-09-03 17:41:21 +0300

    Многоуровневая иерархия

--- src/main/java/schildt/chapter7/TwoDShape.java
+++ src/main/java/schildt/chapter7/TwoDShape.java
@@ -63,36 +63,42 @@ class Triangle extends TwoDShape {
     }
 }
 
-class Shapes5 {
+// Расширение класса Triangle
+class ColorTriangle extends Triangle {
+    private String color;
+
+    ColorTriangle(String c, String s, double w, double h) {
+        super (s, w, h);
+
+        color = c;
+    }
+
+    String getColor() { return color; }
+
+    void showColor() {
+        System.out.println("Цвет - " + color);
+    }
+}
+
+class Shapes6 {
     public static void main(String[] args) {
-        Triangle t1 = new Triangle("закрашенный", 4.0, 4.0);
-        Triangle t2 = new Triangle("Контурныйй", 8.0, 12.0);
-        Triangle t3 = new Triangle (4.0);
-
-        t1 = t2;
-
+        ColorTriangle t1 = new ColorTriangle ("Синий", "контурный", 8.0, 12.0);
+        ColorTriangle t2 = new ColorTriangle ("Красный", "закрашенный", 2.0, 2.0);
 
         System.out.println("Информация о t1: ");
         t1.showStyle();
+
         t1.showDim();
+        t1.showColor();
         System.out.println("Площадь - " + t1.area());
 
         System.out.println();
 
-        System.out.println("Информация о t2: ");
+        System.out.println("Информация o t2: ");
         t2.showStyle();
         t2.showDim();
-
+        t2.showColor();
         System.out.println("Площадь - " + t2.area());
-
-        System.out.println();
-
-        System.out.println("Инфолрмация о t3: ");
-        t3.showStyle();
-        t3.showDim();
-        System.out.println("Площадь - " + t3.area());
-
-        System.out.println();
     }
 }
 
