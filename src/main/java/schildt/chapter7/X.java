3f712d57fe776fc03d672d4f839ad46675eed7b2
alalon369 <alalon211090@gmail.com>
2022-09-05 19:50:31 +0300

    Этот код не пройдет компиляцию, поскольку переменные x и y разных типов.

--- src/main/java/schildt/chapter7/X.java
+++ src/main/java/schildt/chapter7/X.java
@@ -1,36 +1,22 @@
 package schildt.chapter7;
 
-// Обращение к объекту подкласса по ссылочной переменной суперкласса
-
 public class X {
     int a;
 
-    X(int i) { a = i; }
+    X (int i) {a = i; }
 }
-
-class Y extends X {
-    int b;
-
-    Y (int i, int j) {
-        super(j);
-        b = i;
-    }
+class Y {
+    int a;
+    Y (int i) { a = i;}
 }
-
-class SuperSubRef {
+class IncompatibleRef {
     public static void main(String[] args) {
         X x = new X(10);
         X x2;
-        Y y = new Y(5, 6);
+        Y y = new Y(5);
 
         x2 = x;   // Допустимо, поскольку обе переменные одного типа.
-        System.out.println("x2.a: " + x2.a);
 
-        x2 = y;   // По-прежнему допустимо, по указанной выше причине.
-        System.out.println("x2.a: " + x2.a);
-
-        // В классе X известны только члены класса X
-        x2.a = 19;
-        // x2.b = 27;   // Ошибка, так как переменная b не является членом класса X
+        //   x2 = y;   // Ошибка, поскольку переменные разных типов
     }
 }
