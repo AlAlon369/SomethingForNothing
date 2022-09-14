63a059ad2abb53391a3b1e441d65511e780a5332
alalon369 <alalon211090@gmail.com>
2022-09-14 16:22:31 +0300

    Kata 84 solved

---
+++ src/main/java/Codewars/kata84.java
@@ -0,0 +1,28 @@
+package Codewars;
+
+public class kata84 {
+    public static void main(String[] args) {
+        System.out.println(isTriangle(1,2,2));
+        System.out.println(isTriangle(7,2,2));
+    }
+    // Реализуйте функцию, которая принимает 3 целочисленных значения a, b, c.
+    // Функция должна возвращать true, если можно построить треугольник со сторонами заданной длинны.
+    // Функция должна возвращать false, в любом другом случае.
+
+    // examples:
+    // 1,2,2 - true
+    // 7,2,2 - false
+
+
+    public static boolean isTriangle(int a, int b, int c) {
+        if (a < b & a < c & b == c || a + b > c & a + c > b & c + b > a) {
+            return true;
+        /*} else if(a == b || c == a || c == b) {
+            return true;
+        } else if (a != b && b != c && c != a) {
+            return true;
+        }*/
+        }
+        return false;
+    }
+}
