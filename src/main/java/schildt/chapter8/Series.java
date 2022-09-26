04f07535df89dc9bc991275a525160ccf83ffe15
alalon369 <alalon211090@gmail.com>
2022-09-26 20:01:43 +0300

    Реализация интерфейса Series

--- src/main/java/schildt/chapter8/Series.java
+++ src/main/java/schildt/chapter8/Series.java
@@ -5,3 +5,29 @@ public interface Series {
     void reset();   // сброс
     void setStart (int x);   // установка начального значения
 }
+
+// Реализация интерфейса Series
+
+class ByTwos implements Series {
+    int start;
+    int val;
+
+    ByTwos() {
+        start = 0;
+        val = 0;
+    }
+
+    public int getNext() {
+        val += 2;
+        return val;
+    }
+    public void reset() {
+        start = 0;
+        val = 0;
+    }
+
+    public void setStart (int x) {
+        start = x;
+        val = x;
+    }
+}
