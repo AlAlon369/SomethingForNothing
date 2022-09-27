d6d894698904b7b6fd095af1259e281d7b103fb3
alalon369 <alalon211090@gmail.com>
2022-09-27 18:29:29 +0300

    Еще одна реализация интерфейса Series ByThrees

--- src/main/java/schildt/chapter8/Series.java
+++ src/main/java/schildt/chapter8/Series.java
@@ -2,64 +2,60 @@ package schildt.chapter8;
 
 public interface Series {
     int getNext();   // возврат следующего по порядку числа
+
     void reset();   // сброс
-    void setStart (int x);   // установка начального значения
+
+    void setStart(int x);   // установка начального значения
 
 }
 
-// Реализация интерфейса Series
-// Реализация интерфейса Series и добавление метода getPrevious();
-class ByTwos implements Series {
+class ByThrees implements Series {         // Другая реализация интерфейса Series
     int start;
     int val;
-    int prev;
 
-    ByTwos() {
+
+    ByThrees() {
         start = 0;
         val = 0;
-        prev = -2;
     }
 
     public int getNext() {
-        prev = val;
 
-        val += 2;
+        val += 3;
         return val;
     }
+
     public void reset() {
         start = 0;
         val = 0;
-        prev = -2;
     }
 
-    public void setStart (int x) {
+    public void setStart(int x) {
         start = x;
         val = x;
-        prev = x - 2;
+
     }
-    int getPrevious() {
-        return prev;
-    }
+
 }
 
 class SeriesDemo {
     public static void main(String[] args) {
-        ByTwos ob = new ByTwos();
+        ByThrees ob = new ByThrees();
 
-        for (int i = 0; i < 5; i++ ) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Следующее значение: " + ob.getNext());
         }
-            System.out.println("\nСброс");
-            ob.reset();
+        System.out.println("\nСброс");
+        ob.reset();
 
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Следующее значение: " + ob.getNext());
         }
-            System.out.println("\nНачальное значение: 100");
-            ob.setStart(100);
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Следующее значение: " + ob.getNext());
+        System.out.println("\nНачальное значение: 100");
+        ob.setStart(100);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Следующее значение: " + ob.getNext());
 
         }
     }
