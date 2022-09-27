c58124cf7bc84de7213b49111422ae09e2191641
alalon369 <alalon211090@gmail.com>
2022-09-27 18:24:50 +0300

    Bytwos, метод getPrevious()

--- src/main/java/schildt/chapter8/Series.java
+++ src/main/java/schildt/chapter8/Series.java
@@ -4,31 +4,41 @@ public interface Series {
     int getNext();   // возврат следующего по порядку числа
     void reset();   // сброс
     void setStart (int x);   // установка начального значения
+
 }
 
 // Реализация интерфейса Series
-
+// Реализация интерфейса Series и добавление метода getPrevious();
 class ByTwos implements Series {
     int start;
     int val;
+    int prev;
 
     ByTwos() {
         start = 0;
         val = 0;
+        prev = -2;
     }
 
     public int getNext() {
+        prev = val;
+
         val += 2;
         return val;
     }
     public void reset() {
         start = 0;
         val = 0;
+        prev = -2;
     }
 
     public void setStart (int x) {
         start = x;
         val = x;
+        prev = x - 2;
+    }
+    int getPrevious() {
+        return prev;
     }
 }
 
