152847255d78fe2623e3aef47baace4cf45a7334
alalon369 <alalon211090@gmail.com>
2022-09-28 17:09:43 +0300

    класс CircularQueue, реализация кольцевой очереди, которая предназначена для хранения символов

--- src/main/java/schildt/chapter8/IQDemo.java
+++ src/main/java/schildt/chapter8/IQDemo.java
@@ -34,3 +34,40 @@ public void put(char ch) {
     return q[getloc++];
     }
 }
+
+// Кольцевая очередь
+class CircularQueue implements ICharQ {
+    private char q[];   // массив для хранения элементов очереди
+    private int putloc, getloc;   // индексы вставляемых и извлекаемых элементов
+
+    // Создание пустой очереди заданного размера
+    public CircularQueue (int size) {
+        q = new char [size+1];   // Выделение памяти для очереди
+        putloc = getloc = 0;
+    }
+
+    // Помещение символа в очередь
+    public void put(char ch) {
+           // Очередь считается полной, если индекс putloc на единицу меньше индеккса getloc или если индекс putloc
+           // указывает на конец массива, а индекс getloc - на его начало
+        if (putloc + 1 == getloc | ((putloc == q.length - 1) & (getloc == 0))) {
+            System.out.println(" - Очередь заполнена");
+            return;
+            }
+        q[putloc++] = ch;
+        if (putloc == q.length) putloc = 0;   // перейти в начало массива
+    }
+
+    // Извлечение символа из очереди
+    public char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Очередь пуста");
+            return (char) 0;
+        }
+
+        char ch = q[getloc++];
+        if (getloc == q.length) getloc = 0;   // вернуться в начало очереди
+
+        return q[getloc];
+    }
+}
