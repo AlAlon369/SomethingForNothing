50b9b5f22c02bfa8752499e75a9b689746d992ac
alalon369 <alalon211090@gmail.com>
2022-09-29 17:25:19 +0300

    Демонстрация двух роеализаций интерфейса iCharQ: Помещение ряда символов в очередь фиксированного размера. Помещение ряда символов в динамическую очередь

--- src/main/java/schildt/chapter8/IQDemo.java
+++ src/main/java/schildt/chapter8/IQDemo.java
@@ -1,7 +1,44 @@
 package schildt.chapter8;
 
-
+// Демонстрация трех реализаций интерфейса ICharQ
 public class IQDemo {
+    public static void main(String[] args) {
+        FixedQueue q1 = new FixedQueue(10);
+        DynQueue q2 = new DynQueue(5);
+        CircularQueue q3 = new CircularQueue(10);
+
+        ICharQ iQ;
+
+        char ch;
+        int i;
+
+        iQ = q1;
+        // Помещение ряда символов в очередь фиксированного размера
+        for (i = 0; i < 10; i++)
+            iQ.put((char) ('A' + i));
+
+            // Отображение содержимого очереди
+        System.out.print("Содержимое фиксированной очереди: ");
+            for (i = 0; i < 10; i++) {
+                ch = iQ.get();
+                System.out.print(ch);
+            }
+        System.out.println();
+
+            iQ = q2;
+            // Помещение ряда символов в динамическую очередь
+        for (i = 0; i < 10; i++)
+            iQ.put((char) ('Z' - i));
+
+        // Отображение содержимого очереди
+        System.out.print("Содержимое динамической очереди: ");
+        for (i = 0; i < 10; i++) {
+            ch = iQ.get();
+            System.out.print(ch);
+        }
+
+        System.out.println();
+    }
 }
 
 // Динамическая очередь
