fe88e14470c320cdc4409b95de01cc00a122edf3
alalon369 <alalon211090@gmail.com>
2022-10-02 23:42:53 +0300

    Созданный экземпляр класса MyIFImp и использует его для вызова обоих методов, getUserID() и getAdminID().

--- src/main/java/schildt/chapter8/MyIF.java
+++ src/main/java/schildt/chapter8/MyIF.java
@@ -9,3 +9,27 @@ public interface MyIF {
         return 1;
     }
 }
+
+class MyIFImp implements MyIF {
+    // Реализации подлежит лишь метод getUserID() интерфейса MyIF.
+    // Делать это для метода getAdminID() необязательно, поскольку при необходимости может быть использована
+    // его реализация, заданная по умолчанию.
+
+    public int getUserID() {
+        return 100;
+    }
+}
+
+// Использование интерфейсного метода по умолчанию.
+class DefaultMethodDemo {
+    public static void main(String[] args) {
+
+        MyIFImp obj = new MyIFImp();
+
+        // Вызов метода getUserID() возможен, поскольку он явно реализован классом MyIFImp
+        System.out.println("Идентификатор пользователя " + obj.getUserID());
+
+        // Вызов метода getAdminID() также возможен, поскольку представляется его реализация по умолчаниюб
+        System.out.println("Идентификатор администратора: " + obj.getAdminID());
+    }
+}
