ad369267f606670a0f51fd689401315d8e8c13a9
alalon369 <alalon211090@gmail.com>
2022-10-11 23:44:55 +0300

    // Эта программа не будет работать, ArrayIndexOutBoundsException

---
+++ src/main/java/schildt/chapter9/ExcTypeMismatch.java
@@ -0,0 +1,25 @@
+package schildt.chapter9;
+
+// Эта программа не будет работать!
+public class ExcTypeMismatch {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+
+        try {
+            System.out.println("До генерации исключения");
+
+            // Сгенерировать исключение в связи с выходом индекса за пределы массива
+            nums[7] = 10;   // Генерирование исключения ArrayIndexOutBoundsException
+            System.out.println("Эта строка не будет отображаться");
+
+
+            // Исключение, связанное с обращением за пределы массива, нельзя обработать с помощью инструкции catch,
+            // в которой указан тип исключения ArithmeticException
+        }
+            catch (ArithmeticException exc) {
+            // перехватить исключение
+                System.out.println("Выход за пределы массива!");
+        }
+        System.out.println("После инструкции catch");
+    }
+}
