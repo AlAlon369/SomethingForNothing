8432e5c2a7203f83afded0864af9baa4b3ec69bc
alalon369 <alalon211090@gmail.com>
2022-10-23 23:23:15 +0300

    Чтение байтов с клавиатуры в массив

---
+++ src/main/java/schildt/chapter10/ReadBytes.java
@@ -0,0 +1,16 @@
+package schildt.chapter10;
+
+// Чтение байтов с клавиатуры в массив
+import java.io.*;
+
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        byte data[] = new byte[10];
+
+        System.out.println("Введите символы.");
+        System.in.read(data);
+        System.out.print("Вы ввели: ");
+        for (int i = 0; i < data.length; i++)
+            System.out.println((char) data[i]);
+    }
+}
